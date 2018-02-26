137ff5ad1af6b796a2e50bb14028a46f490652d2
whirlwin <whirlwin90@gmail.com>
2018-02-26 14:01:13 +0100

    Add working index page

--- src/main/js/config/ExpressConfig.js
+++ src/main/js/config/ExpressConfig.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 class ExpressConfig {
 
     configure() {
-        return express();
+        const app = express();
+        app.set("view engine", "pug");
+        app.set("views", "src/main/views");
+        app.use("/assets", express.static("src/main/assets/"));
+        return app;
     }
 }
 
